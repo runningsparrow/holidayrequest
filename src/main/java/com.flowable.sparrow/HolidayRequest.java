d87f615cd5a3ce41ae5c5b1c69a64815f2528135
runningsparrow <runningsparrow@gmail.com>
2019-04-27 23:33:17 +0800

    Task

--- src/main/java/com.flowable.sparrow/HolidayRequest.java
+++ src/main/java/com.flowable.sparrow/HolidayRequest.java
@@ -6,7 +6,7 @@ import org.flowable.engine.repository.Deployment;
 import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
 import org.flowable.engine.TaskService;
-
+import org.flowable.task.api.Task;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +16,7 @@ import java.util.Scanner;
 public class HolidayRequest {
     public static void main(String[] args){
 
+        ////创建流程引擎
         ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
                 .setJdbcUrl("jdbc:h2:mem:flowable;DB_CLOSE_DELAY=-1")
                 .setJdbcUsername("sa")
@@ -26,7 +27,7 @@ public class HolidayRequest {
         ProcessEngine processEngine = cfg.buildProcessEngine();
 
 
-
+        ////部署流程定义
         //现在我们已经有了流程BPMN 2.0 XML文件，下来需要将它部署(deploy)到引擎中。部署一个流程定义意味着：
         //流程引擎会将XML文件存储在数据库中，这样可以在需要的时候获取它。
         //流程定义转换为内部的、可执行的对象模型，这样使用它就可以启动流程实例。
@@ -44,7 +45,7 @@ public class HolidayRequest {
         System.out.println("Found process definition : " + processDefinition.getName());
 
 
-
+        ////启动流程实例
         //现在已经在流程引擎中部署了流程定义，因此可以使用这个流程定义作为“蓝图”启动流程实例。
         //要启动流程实例，需要提供一些初始化流程变量。一般来说，可以通过呈现给用户的表单，或者在流程由其他系统自动触发时通过REST API，来获取这些变量。在这个例子里，我们简化为使用java.util.Scanner类在命令行输入一些数据：
 
@@ -98,6 +99,12 @@ public class HolidayRequest {
         Map<String, Object> processVariables = taskService.getVariables(task.getId());
         System.out.println(processVariables.get("employee") + " wants " +
                 processVariables.get("nrOfHolidays") + " of holidays. Do you approve this?");
+
+        //在这里，我们在完成任务时传递带有’approved’变量（这个名字很重要，因为之后会在顺序流的条件中使用！）的map来模拟：
+        boolean approved = scanner.nextLine().toLowerCase().equals("y");
+        variables = new HashMap<String, Object>();
+        variables.put("approved", approved);
+        taskService.complete(task.getId(), variables);
 
     }
 }
